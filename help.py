e4ca4e6fff9decbf14e692938527b44e9a26b1f5
AbyssLink <26038696+AbyssLink@users.noreply.github.com>
2020-08-04 20:09:11 +0800

    add help.py

--- help.py
+++ help.py
@@ -1,19 +1,24 @@
 import os
 
-sub_folder = './docs/'
+path = './docs/'
 
-files = [f for f in os.listdir(sub_folder) if os.path.isfile(sub_folder + f)]
+sub_folders = [f for f in os.listdir(path) if os.path.isdir(path + f)]
 
-print(files)
+sidebar_file = open(os.path.join(path, '_sidebar.md'), 'w')
+for sub_folder in sub_folders:
+	sidebar_file.write(f'* **{sub_folder}**\n')
+	abs_folder = os.path.join(path, sub_folder)
+	files = [f for f in os.listdir(abs_folder) if os.path.isfile(os.path.join(abs_folder, f))]
+	files.sort()
+	print(files)
 
-sidebar_file = open(sub_folder + '_sidebar.md', 'w')
-
-for file in files:
-	if ".md" in file and '_' not in file:
-		with open(os.path.join(sub_folder, file), 'r') as f:
-			first_line = f.readline()
-			name = first_line.replace('# ', '').replace('\n', '')
-			file = file.replace(" ", "%20")
-			sidebar_file.write(f'* [{name}]({file})\n')
+	for file in files:
+		if ".md" in file and '_' not in file:
+			with open(os.path.join(abs_folder, file), 'r') as f:
+				first_line = f.readline()
+				name = first_line.replace('# ', '').replace('\n', '')
+				file = file.replace(" ", "%20")
+				file_link = os.path.join(sub_folder, file)
+				sidebar_file.write(f'	* [{name}]({file_link})\n')
 
 sidebar_file.close()
